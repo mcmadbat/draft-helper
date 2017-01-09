80e9c6dc8ee3980a5e2e86a3dc2f62c745f8a4d8
mcmadbat <mcmadbat@gmail.com>
2017-01-08 20:02:14 -0500

    not working fml

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+let csv = require('fast-csv');
 
 var app = express();
 
@@ -29,12 +30,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', hbs.engine );
 app.set('view engine', 'handlebars');
 
+readPitcherData();
+readBatterData();
+
 // routers
 var index = require('./routes/index');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -114,4 +118,48 @@ function onListening() {
     ? 'pipe ' + addr
     : 'port ' + addr.port;
   console.log('Listening on ' + bind);
+}
+
+function readPitcherData() {
+  let dataObj = {
+    keys : null,
+    data: []
+  };
+
+  csv
+  .fromPath(path.join(__dirname, '/data/pitcher.csv'))
+  .on('data', data => {
+    if (!dataObj.keys){
+      dataObj.keys = data.filter(val => {
+        return val != -1;
+      });
+    } else {
+      dataObj.data.push(data);
+    }
+  })
+  .on('end', () => {
+    app.locals.pitcherData = dataObj;
+  });
+}
+
+function readBatterData() {
+  let dataObj = {
+    keys : null,
+    data: []
+  };
+
+  csv
+  .fromPath(path.join(__dirname, '/data/position.csv'))
+  .on('data', data => {
+    if (!dataObj.keys){
+      dataObj.keys = data.filter(val => {
+        return val != -1;
+      });
+    } else {
+      dataObj.data.push(data);
+    }
+  })
+  .on('end', () => {
+    app.locals.batterData = dataObj;
+  });
 }
