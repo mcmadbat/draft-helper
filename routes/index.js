7d5b0356f03f015873ebb5f5ad27272baebb4816
David Zhang <mcmadbat@gmail.com>
2017-01-10 19:50:37 -0500

    done api for now

--- routes/index.js
+++ routes/index.js
@@ -3,56 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	res.render ('index');
-});
-
-// gets the available fields
-router.get('/pitcherKeys', function(req, res) {
-  return res.status(200).send(req.app.locals.pitcherData.keys);
-});
-
-router.get('/batterKeys', function(req, res) {
-  return res.status(200).send(req.app.locals.batterData.keys);
-});
-
-// returns projection data
-router.get('/pitchers', function(req, res) {
-  let returnData = [];
-
-  if (1 === req.params){
-
-  } else {
-    req.app.locals.pitcherData.data.forEach( arr => {
-      let tmpObj = {};
-      for (var x = 0; x < arr.length; x++ ){
-        tmpObj[req.app.locals.pitcherData.keys[x]] = arr[x];
-      }
-
-      returnData.push(tmpObj);
-    });
-  }
-
-  return res.status(200).send(returnData);
-});
-
-// returns projection data
-router.get('/batters', function(req, res) {
-  let returnData = [];
-
-  if (1 === req.params){
-
-  } else {
-    req.app.locals.batterData.data.forEach( arr => {
-      let tmpObj = {};
-      for (var x = 0; x < arr.length; x++ ){
-        tmpObj[req.app.locals.batterData.keys[x]] = arr[x];
-      }
-
-      returnData.push(tmpObj);
-    });
-  }
-
-  return res.status(200).send(returnData);
+  res.render ('index');
 });
 
 module.exports = router;
