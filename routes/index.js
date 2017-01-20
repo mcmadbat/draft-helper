ab6d5ee0fa1a5f040e19ddb17d02965ae622aeab
David Zhang <mcmadbat@gmail.com>
2017-01-20 17:25:26 -0500

    some ui work

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+// validates the scoring settings
+let validateScoringQuery = req => {
+	let query = req.query;
+	if (0 === Object.keys(query).length) return true;
+
+	let batterKeys = req.app.locals.batterData.keys;
+	let pitcherKeys = req.app.locals.pitcherData.keys;
+
+	// all the scoring settings must correspond to a pitcher or batter stat
+	for (var x in query){
+		if (pitcherKeys[x] || batterKeys[x]){
+			// check if number
+			if (isNaN(query[x])) return false; //scores must be numbers
+		} else {
+			return false;
+		}
+	}
+
+	return true;
+};
+
+const keysToIgnore = ['playerid', 'WAR', 'RA9-WAR', 'FIP', 'ERA', 'Name', 'Team', 
+	'AVG', 'OBP', 'BB/9', 'K/9', 'SLG', 'OPS', 'WHIP', 'wOBA', 'wRC+', 'BsR', 'Fld', 'Off', 'Def'];
+// functor used to ignore playerid in an array
+let filterToIgnore = val => (keysToIgnore.indexOf(val) === -1);
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render ('index');
+	let viewData = {};
+
+	if (!validateScoringQuery(req)) return res.status(500).send('Error: query string invalid');
+	
+	// build view data
+	viewData.query = JSON.stringify(req.query);
+	viewData.batterKeys = JSON.stringify(req.app.locals.batterData.keys.filter(filterToIgnore));
+	viewData.pitcherKeys = JSON.stringify(req.app.locals.pitcherData.keys.filter(filterToIgnore));
+
+  res.render ('index', viewData);
 });
 
 module.exports = router;
